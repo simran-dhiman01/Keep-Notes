cda75106061cf395cf4fab243de1ec87d544436f
simran-dhiman01 <simrankaurdh5@gmail.com>
2025-09-14 21:51:57 +0530

    updates

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ import AdminForm from "../components/AdminForm";
 import Navbar from "../components/Navbar";
 
 const Dashboard = () => {
-  const { user, notes, getNotes, upgradePlan } = useContext(AuthContext);
+  const { user, notes, getNotes, upgradePlan , loading:planLoading } = useContext(AuthContext);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -53,7 +53,7 @@ const Dashboard = () => {
                   className={`w-full font-semibold text-lg rounded-md px-4 py-2 transition-colors 
                   ${user?.tenantId.plan === "pro" ? "bg-gray-300 cursor-not-allowed" : "bg-purple-500 text-white"}`}
                   onClick={upgradePlan}>
-                  {loading
+                  {planLoading
                     ? "Upgrading..."
                     : user?.tenantId.plan === "pro"
                       ? "Pro Plan Active"
