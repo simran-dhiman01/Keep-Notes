b489b493f597aa93f0e52915e77b930d3e3bfe67
simran-dhiman01 <simrankaurdh5@gmail.com>
2025-09-14 21:34:29 +0530

    Added protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,35 @@
-import { Route, Routes } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Health from './components/Health'
+import { AuthContext } from './context/authContext';
+import { useContext } from 'react';
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
+    // if no user → redirect to login
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
         <Route path="/login" element={
           <div className='fixed inset-0 bg-gray-300 flex items-center justify-center'>
             <Login />
           </div>
         } />
-        <Route path='/health' element={<Health />}/>
+        <Route path='/health' element={<Health />} />
       </Routes>
     </>
 
